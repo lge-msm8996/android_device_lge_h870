938ac4bc99b9e7db1e20734f6440409c33bc4fe7
cameron581 <rickle581@gmail.com>
2017-08-21 04:14:16 -0400

    h870:du'ify

--- du.mk
+++ du.mk
@@ -1,12 +1,12 @@
 $(call inherit-product, device/lge/h870/full_h870.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Overlays (inherit after vendor/cm to ensure we override it)
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-PRODUCT_NAME := lineage_h870
+PRODUCT_NAME := du_h870
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g6" \
